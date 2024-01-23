b5fb280527535c4721ae3dc83b8b3e5e3738eb16
Kokutaiji-solution <109887796+Kokutaiji-solution@users.noreply.github.com>
2024-01-23 17:03:40 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+function main() {
+
+}
+
+// クリックしたら音を鳴らす
+function clickSound(option) {
+    document.addEventListener('mousedown', function (mouseEvent) {
+        var tempSound = new Audio();
+        console.log(mouseEvent.button)
+        if (mouseEvent.button == 0) {
+            if (option.buttons.left) {
+                tempSound.src = option.buttons.left.src;
+            } else {
+                tempSound.src = option.default.src;
+            }
+        } else if (mouseEvent.button == 1) {
+            if (option.buttons.center) {
+                tempSound.src = option.buttons.center.src;
+            } else {
+                tempSound.src = option.default.src;
+            }
+        } else if (mouseEvent.button == 2) {
+            if (option.buttons.right) {
+                tempSound.src = option.buttons.right.src
+            } else {
+                tempSound.src = option.default.src
+            }
+        }
+        tempSound.play();
+    })
+}
+
+// キーを押したら音を鳴らす
+function keyboardSound(option) {
+    window.onkeydown = function (e) {
+        const tempSound = new Audio();
+        console.log(e.key)
+        if (option.keys[e.key]) {
+            tempSound.src = option.keys[e.key].src;
+        } else {
+            tempSound.src = option.default.src;
+        }
+        tempSound.currentTime = 0;
+        tempSound.play();
+
+    }
+}
+
+// クリックしたときにパーティクルを表示
+function clickParticle() {
+
+}
+
+// キーを押したらパーティクルを表示
+function keyboardParticle() {
+
+}
